c255a14d5df65a325a6b5bdd9310c90b5646b054
Jože Ws <joze.ws11@gmail.com>
2019-01-15 17:46:56 +0100

    updates filenames

--- ru-ipa.py
+++ ru-ipa.py
@@ -5,7 +5,11 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 
-baseurl = "https://easypronunciation.com/en/russian-phonetic-transcription-converter#phonetic_transcription"
+#baseurl = "https://easypronunciation.com/en/russian-phonetic-transcription-converter#phonetic_transcription"
+#mydriver = webdriver.Firefox()
+#mydriver.get(baseurl)
 
-mydriver = webdriver.Firefox()
-mydriver.get(baseurl)
+file = open("ru-nouns.txt", "r+") 
+lines = [line.replace("masculine", "m").replace("feminine", "f").replace("neuter", "n") for line in file]
+text = "".join(lines)
+file.write(text)
